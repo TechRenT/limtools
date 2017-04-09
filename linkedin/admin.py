b7779da6accd91655e461eb451b9051ba5ca6820
Renelle Tigue <titingrenelle@gmail.com>
2017-04-10 05:24:22 +0800

    add import-export feature for LinkedinProfile model

--- linkedin/admin.py
+++ linkedin/admin.py
@@ -1,6 +1,17 @@
 from django.contrib import admin
+from import_export import resources
+from import_export.admin import ImportExportModelAdmin
 
 from . import models
 
-# Register your models here.
-admin.site.register(models.LinkedinProfile)
+# So we can be able to import and export data for LinkedinProfile class
+class LinkedinResource(resources.ModelResource):
+
+    class Meta:
+        model = models.LinkedinProfile
+        fields = ('id', 'profile_link')
+
+
+@admin.register(models.LinkedinProfile)
+class LinkedinProfileAdmin(ImportExportModelAdmin):
+    resource_class = LinkedinResource
