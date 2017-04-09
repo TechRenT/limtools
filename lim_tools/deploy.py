f27392750410681c1e19253bb7a2bb08e0c28852
Renelle Tigue <titingrenelle@gmail.com>
2017-04-10 05:51:04 +0800

    add the files PythonAnywhere needs to deploy

---
+++ lim_tools/deploy.py
@@ -0,0 +1,24 @@
+"""
+WSGI config for lim_tools project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/1.9/howto/deployment/wsgi/
+"""
+
+import os
+import sys
+
+from django.core.wsgi import get_wsgi_application
+
+from whitenoise.django import DjangoWhiteNoise
+
+path = "home/limtools/limtools"
+if path not in sys.path:
+    sys.path.append(path)
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "lim_tools.settings")
+
+application = get_wsgi_application()
+application = DjangoWhiteNoise(application)
