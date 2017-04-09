c646b6fabddc50ece58c54dfdf1740bb84b0d4f1
Renelle Tigue <titingrenelle@gmail.com>
2017-04-10 06:15:17 +0800

    add deploy settings

---
+++ lim_tools/deploy_settings/__init__.py
@@ -0,0 +1,11 @@
+from ..settings import *
+
+DEBUG = False
+TEMPLATE_DEBUG = DEBUG
+
+ALLOWED_HOSTS = [
+    'localhost',
+    '.pythonanywhere.com',
+]
+
+SECRET_KEY = get_env_variable("SECRET_KEY")
