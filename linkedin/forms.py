7ad328fa4602a5d38a4fa73a4a6ca57bda79d978
Renelle Tigue <titingrenelle@gmail.com>
2017-04-12 00:33:47 +0800

    add form validation error if a profile link ends with slash

--- linkedin/forms.py
+++ linkedin/forms.py
@@ -15,5 +15,7 @@ class LinkedinProfileForm(forms.ModelForm):
 
         profile_list = [str(profile) for profile in models.LinkedinProfile.objects.all()]
 
+        if profile_link[-1] == '/':
+            raise forms.ValidationError("Profile link must not end with slash!")
         if profile_link in profile_list:
             raise forms.ValidationError("This profile already exist in our database!")
